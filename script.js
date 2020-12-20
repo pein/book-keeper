2a1f6acaeb4fd06f446d1c253ee00e57e1e8884c
Ilyes Houha <mi.houha@gmail.com>
2020-12-20 23:07:42 +0100

    Form validation

--- script.js
+++ script.js
@@ -9,9 +9,36 @@ const bookmarksContainer = document.getElementById("bookmarks-container");
 // Show Modal, Focus on input
 function showModal() {
   modal.classList.add("show-modal");
-  websiteNameEL.focus();
 }
 
+// Validate form
+function validate(nameValue, urlValue) {
+  const expresion = /(?:http[s]?\/\/)?(?:[\w\-]+(?::[\w\-]+)?@)?(?:[\w\-]+\.)+(?:[a-z]{2,4})(?::[0-9]+)?(?:\/[\w\-\.%]+)*(?:\?(?:[\w\-\.%]+=[\w\-\.%!]+&?)+)?(#\w+\-\.%!)?/g;
+  const regex = new RegExp(expresion);
+  if (!nameValue || !urlValue) {
+    alert("Please submit valid vlues for both fields");
+    return false;
+  }
+  if (!urlValue.match(regex)) {
+    alert("Enter  a valid url");
+    return false;
+  }
+
+  return true;
+}
+
+// Handel Data from form
+function storebookmark(e) {
+  e.preventDefault();
+  const nameValue = websiteNameEL.value;
+  let urlValue = websiteUrlEL.value;
+  if (!urlValue.includes("http://", "https://")) {
+    urlValue = `https://${urlValue}`;
+  }
+  if (!validate(nameValue, urlValue)) {
+    return false;
+  }
+}
 // Event Listners
 modalShow.addEventListener("click", showModal);
 modalClose.addEventListener("click", () => {
@@ -20,3 +47,5 @@ modalClose.addEventListener("click", () => {
 window.addEventListener("click", (e) => {
   e.target == modal ? modal.classList.remove("show-modal") : false;
 });
+
+bookmarkForm.addEventListener("submit", storebookmark);
